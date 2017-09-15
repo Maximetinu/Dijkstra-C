96421bfc8fdc2187e28908b298d8b4b5e901595b
Maximetinu <maximetinu@gmail.com>
2017-09-15 20:39:36 +0200

    First dijkstra header approach

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
+
+int nodes_count;
+
+void dijkstra(int adjacency_matrix[nodes_count][nodes_count],int start_node, int end_node);
 
 int main() {
-    // Changed project title
-    printf("Hello, World!\n");
-    return 0;
+
+}
+
+void dijkstra(int adjacency_matrix[nodes_count][nodes_count],int start_node, int end_node)
+{
+    // the idea is to return an array containing the shortest path from startnode to endnode, in such a way that array[0] will be the start_node and array[sizeof(array)-1] will be the end_node.
+    // would be useful to return also the total distance of the path. Or could code a helper functions getDistance(array[])
 }
