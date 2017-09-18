34787c6a49a9ea9bcf9d185e659efedc7bcc053a
Maximetinu <maximetinu@gmail.com>
2017-09-18 18:09:50 +0200

    Separated logic from IO.
    Now working with structs.

--- main.c
+++ main.c
@@ -1,72 +1,37 @@
+//
+// Created by metinu - Miguel Medina Ballesteros - on September 2017. maximetinu@gmail.com
+//
 #include<stdio.h>
 #include <stdbool.h>
 
 #define INFINITY 9999
 #define MAX 10
 
-struct Path dijkstra(int adjacency[MAX][MAX], int nodes, int starting_node, int target_node);
-
-struct Path {
+struct Dijkstra_output {
     int distance;
     int path[MAX];
     int size;
-};
-
-int main() {
+}; // struct descripting a path, wich will be the shortest path calculated by Dijkstra
+struct Dijkstra_input {
     int adjacency[MAX][MAX];
     int nodes;
     int starting_node;
     int target_node;
+}; // algorithm input
 
-    do {
-        printf("Enter no. of vertices:");
-        scanf("%d", &nodes);
-    } while (nodes > MAX || nodes <= 1);
+struct Dijkstra_output dijkstra(struct Dijkstra_input input);
+struct Dijkstra_input input_by_terminal(); // takes the input by keyboard using scanf
+void print_dijkstra_ouput(struct Dijkstra_output output); // prints the input in terminal using printf
 
-
-    bool self_distance_valid = true;
-    printf("\nEnter the adjacency matrix:\n");
-    for (int i = 0; i < nodes; i++)
-        for (int j = 0; j < nodes; j++) {
-            scanf("%d", &adjacency[i][j]);
-            if (i == j && adjacency[i][j] != 0) {
-                self_distance_valid = false;
-                adjacency[i][j] = 0;
-            }
-        }
-
-    if (!self_distance_valid) {
-        printf("\nA distance between a node to himself is not zero.\nThe adjacency matrix has been corrected to: \n");
-        for (int i = 0; i < nodes; i++) {
-            printf("\n");
-            for (int j = 0; j < nodes; j++)
-                printf("%d ", adjacency[i][j]);
-        }
-        printf("\n");
-    }
-
-    do {
-        printf("\nEnter the starting node:");
-        scanf("%d", &starting_node);
-    } while (starting_node > nodes || starting_node < 0);
-
-    do {
-        printf("\nEnter the target node:");
-        scanf("%d", &target_node);
-    } while (target_node >= nodes || target_node < 0 || target_node == starting_node);
-
-    struct Path shortest_path = dijkstra(adjacency, nodes, starting_node, target_node);
-
-    printf("\nThe Dijkstra shortest path is: %d", shortest_path.path[0]);
-    for (int i = 1; i < shortest_path.size; i++) {
-        printf("-->%d", shortest_path.path[i]);
-    }
-    printf("\nPath total distance: %d", shortest_path.distance);
+int main() {
+    struct Dijkstra_input input = input_by_terminal();
+    struct Dijkstra_output shortest_path = dijkstra(input);
+    print_dijkstra_ouput(shortest_path);
 
     return 0;
 }
 
-struct Path dijkstra(int adjacency[MAX][MAX], int nodes, int starting_node, int target_node) {
+struct Dijkstra_output dijkstra(struct Dijkstra_input input) {
 
     int costs[MAX][MAX];
     int distances_to_source[MAX];
@@ -83,33 +48,32 @@ struct Path dijkstra(int adjacency[MAX][MAX], int nodes, int starting_node, int 
     // count stores the number of nodes seen so far
     // min_distance will store the minimum distance from the source already found for the current path
 
-
     // create the cost matrix from the adjacency matrix, setting reflective costs to infinity to avoid choose them
-    for (int i = 0; i < nodes; i++)
-        for (int j = 0; j < nodes; j++)
-            if (adjacency[i][j] == 0)
+    for (int i = 0; i < input.nodes; i++)
+        for (int j = 0; j < input.nodes; j++)
+            if (input.adjacency[i][j] == 0)
                 costs[i][j] = INFINITY;
             else
-                costs[i][j] = adjacency[i][j];
+                costs[i][j] = input.adjacency[i][j];
 
     // initializing predecessors[] to starting node, distances_to_source[i] to cost between i and start and visited[] to false
-    for (int i = 0; i < nodes; i++) {
-        distances_to_source[i] = costs[starting_node][i];
-        predecessors[i] = starting_node;
+    for (int i = 0; i < input.nodes; i++) {
+        distances_to_source[i] = costs[input.starting_node][i];
+        predecessors[i] = input.starting_node;
         visited[i] = false;
     }
 
     // initializing the source as already visited and distance to himself to zero
-    distances_to_source[starting_node] = 0;
-    visited[starting_node] = true;
+    distances_to_source[input.starting_node] = 0;
+    visited[input.starting_node] = true;
     count = 1;
 
     // for each possible neighbour
-    while (count < nodes - 1) {
+    while (count < input.nodes - 1) {
         min_distance = INFINITY;
 
         // looping neighbours nodes except the already visited
-        for (int i = 0; i < nodes; i++)
+        for (int i = 0; i < input.nodes; i++)
             if (distances_to_source[i] < min_distance && !visited[i]) {
                 min_distance = distances_to_source[i];
                 next_node = i;
@@ -119,7 +83,7 @@ struct Path dijkstra(int adjacency[MAX][MAX], int nodes, int starting_node, int 
         visited[next_node] = true;
 
         // choosing next_node by checking the one with minimum distance to source
-        for (int i = 0; i < nodes; i++)
+        for (int i = 0; i < input.nodes; i++)
             if (!visited[i])
                 if (min_distance + costs[next_node][i] < distances_to_source[i]) {
                     distances_to_source[i] = min_distance + costs[next_node][i];
@@ -129,18 +93,18 @@ struct Path dijkstra(int adjacency[MAX][MAX], int nodes, int starting_node, int 
     }
 
     // building shortest_path in solution struct
-    struct Path solution;
+    struct Dijkstra_output solution;
 
-    solution.distance = distances_to_source[target_node];
-    solution.path[0] = target_node;
+    solution.distance = distances_to_source[input.target_node];
+    solution.path[0] = input.target_node;
     solution.size = 1;
 
     // filling path with predecessors
-    for (int i = target_node; i != starting_node; solution.size++, i = predecessors[i])
+    for (int i = input.target_node; i != input.starting_node; solution.size++, i = predecessors[i])
         solution.path[solution.size] = predecessors[i];
 
     // reverting path (predecessors) to make a path from source(0) to target(size - 1)
-    for (int temp, end = solution.size - 1, i = 0; i < nodes / 2; i++) {
+    for (int temp, end = solution.size - 1, i = 0; i < input.nodes / 2; i++) {
         temp = solution.path[i];
         solution.path[i] = solution.path[end];
         solution.path[end] = temp;
@@ -150,3 +114,56 @@ struct Path dijkstra(int adjacency[MAX][MAX], int nodes, int starting_node, int 
     // returning shortest_path calculated with Dijkstra algorithm as Path struct
     return solution;
 }
+struct Dijkstra_input input_by_terminal() {
+    struct Dijkstra_input input;
+    do {
+        printf("Enter no. of vertices:");
+        scanf("%d", &input.nodes);
+    } while (input.nodes > MAX || input.nodes <= 1);
+
+
+    bool self_distance_valid = true;
+    printf("\nEnter the adjacency matrix:\n");
+    for (int i = 0; i < input.nodes; i++)
+        for (int j = 0; j < input.nodes; j++) {
+            scanf("%d", &input.adjacency[i][j]);
+            if (i == j && input.adjacency[i][j] != 0) {
+                self_distance_valid = false;
+                input.adjacency[i][j] = 0;
+            }
+        }
+
+    if (!self_distance_valid) {
+        printf("\nA distance between a node to himself is not zero.\nThe adjacency matrix has been corrected to: \n");
+        for (int i = 0; i < input.nodes; i++) {
+            printf("\n");
+            for (int j = 0; j < input.nodes; j++)
+                printf("%d ", input.adjacency[i][j]);
+        }
+        printf("\n");
+    }
+
+    do {
+        printf("\nEnter the starting node:");
+        scanf("%d", &input.starting_node);
+    } while (input.starting_node > input.nodes || input.starting_node < 0);
+
+    do {
+        printf("\nEnter the target node:");
+        scanf("%d", &input.target_node);
+    } while (input.target_node >= input.nodes || input.target_node < 0 || input.target_node == input.starting_node);
+    return input;
+}
+void print_dijkstra_ouput(struct Dijkstra_output output) {
+    printf("\nThe Dijkstra shortest path is: %d", output.path[0]);
+    for (int i = 1; i < output.size; i++) {
+        printf("-->%d", output.path[i]);
+    }
+    printf("\nPath total distance: %d", output.distance);
+}
+
+// TODO: if (args) input_by_file(filename)
+// TODO: print_dijkstra_input - for checking the file reading
+// TODO: text_output_to_file(filename)
+// TODO: graphic_output_to_[window][plotting graph][image]
+// TODO: separate files between .h and .c
